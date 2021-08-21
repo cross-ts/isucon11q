8b2172bf545ccc83f832e60825ae099756c9ce7a
cross-ts <ts.kouhei@gmail.com>
2021-08-21 15:32:50 +0900

    remove

--- isucon/ruby/app.rb
+++ isucon/ruby/app.rb
@@ -178,30 +178,6 @@ module Isucondition
         else
           return false
         end
-        # keys = %w(is_dirty= is_overweight= is_broken=)
-        # value_true = 'true'
-        # value_false = 'false'
-
-        # idx_cond_str = 0
-        # keys.each_with_index do |key, idx_keys|
-        #   return false unless condition_str[idx_cond_str..-1].start_with?(key)
-        #   idx_cond_str += key.size
-        #   case
-        #   when condition_str[idx_cond_str..-1].start_with?(value_true)
-        #     idx_cond_str += value_true.size
-        #   when condition_str[idx_cond_str..-1].start_with?(value_false)
-        #     idx_cond_str += value_false.size
-        #   else
-        #     return false
-        #   end
-
-        #   if idx_keys < (keys.size-1)
-        #     return false unless condition_str[idx_cond_str] == ?,
-        #     idx_cond_str += 1
-        #   end
-        # end
-
-        # idx_cond_str == condition_str.size
       end
     end
 
