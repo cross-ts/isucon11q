8a584b064ff57b940ab7c34e63a8169b809fda32
cross-ts <ts.kouhei@gmail.com>
2021-08-21 14:18:15 +0900

    MYSQL_HOSTを変更

--- isucon/bot.sh
+++ isucon/bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-  mysql -pisucon -uisucon -A isucondition -e "LOAD DATA LOCAL INFILE '/dev/shm/condition.csv' INTO TABLE isucondition.isu_condition FIELDS TERMINATED BY ',' ENCLOSED BY '\"' (\`jia_isu_uuid\`, \`timestamp\`, \`is_sitting\`, \`condition\`, \`message\`);"
+  mysql -h${MYSQL_HOST} -pisucon -uisucon -A isucondition -e "LOAD DATA LOCAL INFILE '/dev/shm/condition.csv' INTO TABLE isucondition.isu_condition FIELDS TERMINATED BY ',' ENCLOSED BY '\"' (\`jia_isu_uuid\`, \`timestamp\`, \`is_sitting\`, \`condition\`, \`message\`);"
   rm -f /dev/shm/condition.csv
   sleep 1
 done
