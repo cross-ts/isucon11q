86b08da639bba774d7cddb97451367da2eb803fd
cross-ts <ts.kouhei@gmail.com>
2021-08-21 15:45:15 +0900

    Fix

--- isucon/ruby/app.rb
+++ isucon/ruby/app.rb
@@ -599,13 +599,13 @@ module Isucondition
     # ISUの性格毎の最新のコンディション情報
     get '/api/trend' do
       res = CHARACTERS.map do |character|
-        isu_list = db.xquery('SELECT * FROM `isu` WHERE `character` = ?', character.fetch(:character))
+        isu_list = db.xquery('SELECT * FROM `isu` WHERE `character` = ?', character)
         character_info_isu_conditions = []
         character_warning_isu_conditions = []
         character_critical_isu_conditions = []
 
         isu_list.each do |isu|
-          conditions = db.xquery('SELECT `condition`, `timestamp` FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC', isu.fetch(:jia_isu_uuid)).to_a
+          conditions = db.xquery('SELECT `condition`, `timestamp` FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC LIMIT 1', isu.fetch(:jia_isu_uuid)).to_a
           unless conditions.empty?
             isu_last_condition = conditions.first
             condition_level = calculate_condition_level(isu_last_condition.fetch(:condition))
@@ -626,7 +626,7 @@ module Isucondition
         character_critical_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
 
         {
-          character: character.fetch(:character),
+          character: character,
           info: character_info_isu_conditions,
           warning: character_warning_isu_conditions,
           critical: character_critical_isu_conditions,
