fe440d113244a2709c837fab80843a8ca328b763
cross-ts <ts.kouhei@gmail.com>
2021-08-21 13:49:13 +0900

    add shebang

--- isucon/bot.sh
+++ isucon/bot.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 while true; do
   mysql -pisucon -uisucon -A isucondition -e "LOAD DATA LOCAL INFILE '/dev/shm/condition.csv' INTO TABLE isucondition.isu_condition FIELDS TERMINATED BY ',' ENCLOSED BY '\"' (\`jia_isu_uuid\`, \`timestamp\`, \`is_sitting\`, \`condition\`, \`message\`);"
   sleep 1
